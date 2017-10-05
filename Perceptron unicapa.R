7c0cd02ff827bcfd4c951efa5bff1ac6b49a91c9
Raul Guzman <rguzmanc.161@gmail.com>
2017-10-05 09:29:52 -0500

    update single layer perceptron

--- Perceptron unicapa.R
+++ Perceptron unicapa.R
@@ -11,32 +11,33 @@
 # 1 |  1   |   0
 
 hardlim <-function(x) {
-    y <- ifelse(x >= 0, 1, 0)
+  y <- ifelse(x >= 0, 1, 0)
 }
 
-W <- c(1,-0.3,-0.2)
+W <- c(-0.7,-0.3,-0.2)
 X <- matrix(
   c(-1,0,0,
     -1,0,1,
     -1,1,0,
     -1,1,1),
   nrow = 4,
-  ncol = 3
+  ncol = 3,
+  byrow = TRUE
 )
 
-T <- c(1,1,1,0)
+t <- c(1,1,1,0)
 
 n <- 0.1
-h <- -0.7
 
-y <- 0
+e <- c()
 
-for (epoch in 0:100){
-  for (q in 0:3){
-    if(t[q])
+for (epoch in 1:2){
+  for (q in 1:4){
+    e[q] <- t[q] - hardlim(sum(X[q,]*W))
+    W <- W + X[q,]*n*e[q]
   }
 }
-X[1,]
 
-
+print(e)
+print(W)
 
